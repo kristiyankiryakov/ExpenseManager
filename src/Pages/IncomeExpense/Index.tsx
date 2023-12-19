7dc4b914e06995be7b63301f4a47a2ebbb840dda
Kristiyan Kiryakov <emineo@abv.bg>
2023-12-19 15:51:57 +0200

    filter cats hook

--- src/Pages/IncomeExpense/Index.tsx
+++ src/Pages/IncomeExpense/Index.tsx
@@ -12,33 +12,33 @@ import CreateCategoryBtn from './CreateCategoryBtn.tsx';
 import FilterCategories from './FilterCategories.tsx';
 import PageSwitch from './PageSwitch.tsx';
 import SingleExpense from './SingleExpense.tsx';
+import useFilteredCats from '../../hooks/useFilteredCats.ts';
 
 const Index = () => {
     const {userCategories, selectCategory, selectedCategory, setSelectedCategory, addCategory, setNewCategory} = useCategories();
     const {dailyExpenses, addExpense, amount, setAmount, setSelectedDate} = useExpenses({period: Period.DAY, userCategories, selectedCategory, setSelectedCategory});
-
+    
     const [pageSwitch, setPageSwitch] = useState<Page>(Page.EXPENSE);
     const [filter, setFilter] = useState("");
     const [openModal, setOpenModal] = useState<string | undefined>();
+
+    const filteredCats = useFilteredCats({userCategories, filter});
     const isExpensePage = pageSwitch === Page.EXPENSE;
 
     return (
-        <main className="bg-slate-900 flex flex-col h-screen justify-between">
+        <main className="bg-slate-900 flex flex-col h-screen justify-between">;
 
             <PageSwitch setPageSwitch={setPageSwitch} isExpensePage={isExpensePage} />
 
             <div className="flex-col space-y-10" >
 
                 <section className={`flex w-[95%] justify-around m-auto mt-10`}>
-
                     <Amount amount={amount} setAmount={setAmount} />
-
                     <FilterCategories filter={filter} setFilter={setFilter} />
-
                 </section>
 
                 <div className="grid grid-cols-4 w-[95%] m-auto border border-gray-600 rounded-xl bg-slate-800 h-44 overflow-y-auto scroll-smooth" >
-                    {userCategories && userCategories.map((category, i) => {
+                    {filteredCats && filteredCats.map((category, i) => {
                         return (
                             <div onClick={() => selectCategory(i)} id="2344" key={category.name} className="flex-col space-y-1 justify-center items-center my-2" >
                                 <div tabIndex={0} className={`p-2 rounded-lg w-fit m-auto bg-lime-500 focus:ring-4 focus:outline-none focus:ring-lime-300`} >
@@ -50,9 +50,7 @@ const Index = () => {
                             </div>
                         )
                     })}
-
                     <CreateCategoryBtn setOpenModal={setOpenModal} />
-
                 </div>
             </div>
 
@@ -64,7 +62,7 @@ const Index = () => {
 
             <section className={`mb-4 w-full mx-auto h-60 max-h-80 overflow-y-auto`} >
                 <p className="text-right text-stone-400 pr-3">{isExpensePage ? 'Daily Expenses:' : 'Monthly Income'}</p>
-                
+
                 {(isExpensePage && dailyExpenses) && dailyExpenses.map((expense, i) => {
                     return <SingleExpense expense={expense} key={i} />;
                 })}
---
+++ src/hooks/useFilteredCats.ts
@@ -0,0 +1,21 @@
+import {useMemo} from "react";
+import Category from "../interfaces/Category";
+
+
+type Props = {
+    userCategories: Category[] | null
+    filter: string
+}
+
+function useFilteredCats({userCategories, filter}: Props) {
+
+    const filteredData = useMemo(() => {
+        if (userCategories) {
+            return userCategories.filter(cat => cat.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()));
+        }
+    }, [userCategories, filter]);
+
+    return filteredData;
+}
+
+export default useFilteredCats
