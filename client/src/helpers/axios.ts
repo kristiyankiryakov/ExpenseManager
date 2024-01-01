87d15b6e872e66febbecfbdd2fd586df5ae7f78d
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-01-01 11:06:54 +0200

    change base url

--- client/src/helpers/axios.ts
+++ client/src/helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3500',
+  baseURL: 'https://expense-manager-server.vercel.app',
   withCredentials: true,
 });
 
