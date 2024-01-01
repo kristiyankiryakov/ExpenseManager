53da08103339f8315e2aaea648e3cb84cc0139d3
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-01-01 09:58:48 +0200

    change base url

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://expense-manager-ivory.vercel.app/',
+  baseURL: 'https://backend-express-manager.vercel.app',
   withCredentials: true,
 });
 
